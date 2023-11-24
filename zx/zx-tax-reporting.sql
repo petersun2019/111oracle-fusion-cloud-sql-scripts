52ef347ecfc98ab700b0fafb4eab8e3598359b55
throwing-cheese <143490562+throwing-cheese@users.noreply.github.com>
2023-11-24 13:48:15 +0000

    Update zx-tax-reporting.sql

--- zx/zx-tax-reporting.sql
+++ zx/zx-tax-reporting.sql
@@ -14,6 +14,7 @@ Queries:
 -- JE_ZZ_VAT_REP_TRX_T - VERSION 2 (SMALLER VERSION)
 -- JG_ZZ_VAT_TRX_DETAILS - VERSION 1
 -- JG_ZZ_VAT_TRX_DETAILS - VERSION 2
+-- TAX BOX SETUP
 
 */
 
@@ -271,3 +272,178 @@ It can be used as a source for audits to provide information reported to the tax
 			 , trx.rep_context_entity_name
 			 , trx.created_by
 	  order by trx.request_id desc
+
+-- ##################################################################
+-- TAX BOX SETUP
+-- ##################################################################
+
+/*
+https://community.oracle.com/customerconnect/discussion/707251/how-can-i-make-a-list-of-the-tax-box-allocation-logic
+*/
+
+		select (select zx_rules_b.tax_rule_code
+				  from zx_rules_b 
+				 where zx_rules_b.service_type_code = 'DET_TAX_BOX' 
+				   and zx_rules_b.enabled_flag = 'Y'
+				   and zx_rules_b.det_factor_templ_code = zx_condition.det_factor_templ_code
+				   and rownum = 1) tax_rule
+			 , zx_condition.det_factor_templ_code
+			 , zx_condition.condition_group_code
+			 , zx_condition.priority
+			 , zx_condition.determining_factor_code1
+			 , zx_condition.tax_parameter_code1
+			 , zx_condition.determining_factor_class1_code
+			 , zx_condition.operator1_code
+			 , zx_condition.numeric_value1
+			 , zx_condition.date_value1
+			 , zx_condition.alphanumeric_value1
+			 , zx_condition.determining_factor_code2
+			 , zx_condition.tax_parameter_code2
+			 , zx_condition.determining_factor_class2_code
+			 , zx_condition.operator2_code
+			 , zx_condition.numeric_value2
+			 , zx_condition.date_value2
+			 , zx_condition.alphanumeric_value2
+			 , zx_condition.determining_factor_code3
+			 , zx_condition.tax_parameter_code3
+			 , zx_condition.determining_factor_class3_code
+			 , zx_condition.operator3_code
+			 , zx_condition.numeric_value3
+			 , zx_condition.date_value3
+			 , zx_condition.alphanumeric_value3
+			 , zx_condition.determining_factor_code4
+			 , zx_condition.tax_parameter_code4
+			 , zx_condition.determining_factor_class4_code
+			 , zx_condition.operator4_code
+			 , zx_condition.numeric_value4
+			 , zx_condition.date_value4
+			 , zx_condition.alphanumeric_value4
+			 , zx_condition.determining_factor_code5
+			 , zx_condition.tax_parameter_code5
+			 , zx_condition.determining_factor_class5_code
+			 , zx_condition.operator5_code
+			 , zx_condition.numeric_value5
+			 , zx_condition.date_value5
+			 , zx_condition.alphanumeric_value5
+			 , zx_condition.determining_factor_code6
+			 , zx_condition.tax_parameter_code6
+			 , zx_condition.determining_factor_class6_code
+			 , zx_condition.operator6_code
+			 , zx_condition.numeric_value6
+			 , zx_condition.date_value6
+			 , zx_condition.alphanumeric_value6
+			 , zx_condition.determining_factor_code7
+			 , zx_condition.tax_parameter_code7
+			 , zx_condition.determining_factor_class7_code
+			 , zx_condition.operator7_code
+			 , zx_condition.numeric_value7
+			 , zx_condition.date_value7
+			 , zx_condition.alphanumeric_value7
+			 , zx_condition.determining_factor_code8
+			 , zx_condition.tax_parameter_code8
+			 , zx_condition.determining_factor_class8_code
+			 , zx_condition.operator8_code
+			 , zx_condition.numeric_value8
+			 , zx_condition.date_value8
+			 , zx_condition.alphanumeric_value8
+			 , zx_condition.determining_factor_code9
+			 , zx_condition.tax_parameter_code9
+			 , zx_condition.determining_factor_class9_code
+			 , zx_condition.operator9_code
+			 , zx_condition.numeric_value9
+			 , zx_condition.date_value9
+			 , zx_condition.alphanumeric_value9
+			 , zx_condition.determining_factor_code10
+			 , zx_condition.tax_parameter_code10
+			 , zx_condition.determining_factor_clas10_code
+			 , zx_condition.operator10_code
+			 , zx_condition.numeric_value10
+			 , zx_condition.date_value10
+			 , zx_condition.alphanumeric_value10
+			 , zx_condition.alphanumeric_value
+		  from (select conditiongroupspeo.det_factor_templ_code
+					 , conditiongroupspeo.condition_group_code
+					 , processresutspeo.priority
+					 , conditiongroupspeo.determining_factor_code1
+					 , conditiongroupspeo.tax_parameter_code1
+					 , conditiongroupspeo.determining_factor_class1_code
+					 , conditiongroupspeo.operator1_code
+					 , conditiongroupspeo.numeric_value1
+					 , conditiongroupspeo.date_value1
+					 , conditiongroupspeo.alphanumeric_value1
+					 , conditiongroupspeo.determining_factor_code2
+					 , conditiongroupspeo.tax_parameter_code2
+					 , conditiongroupspeo.determining_factor_class2_code
+					 , conditiongroupspeo.operator2_code
+					 , conditiongroupspeo.numeric_value2
+					 , conditiongroupspeo.date_value2
+					 , conditiongroupspeo.alphanumeric_value2
+					 , conditiongroupspeo.determining_factor_code3
+					 , conditiongroupspeo.tax_parameter_code3
+					 , conditiongroupspeo.determining_factor_class3_code
+					 , conditiongroupspeo.operator3_code
+					 , conditiongroupspeo.numeric_value3
+					 , conditiongroupspeo.date_value3
+					 , conditiongroupspeo.alphanumeric_value3
+					 , conditiongroupspeo.determining_factor_code4
+					 , conditiongroupspeo.tax_parameter_code4
+					 , conditiongroupspeo.determining_factor_class4_code
+					 , conditiongroupspeo.operator4_code
+					 , conditiongroupspeo.numeric_value4
+					 , conditiongroupspeo.date_value4
+					 , conditiongroupspeo.alphanumeric_value4
+					 , conditiongroupspeo.determining_factor_code5
+					 , conditiongroupspeo.tax_parameter_code5
+					 , conditiongroupspeo.determining_factor_class5_code
+					 , conditiongroupspeo.operator5_code
+					 , conditiongroupspeo.numeric_value5
+					 , conditiongroupspeo.date_value5
+					 , conditiongroupspeo.alphanumeric_value5
+					 , conditiongroupspeo.determining_factor_code6
+					 , conditiongroupspeo.tax_parameter_code6
+					 , conditiongroupspeo.determining_factor_class6_code
+					 , conditiongroupspeo.operator6_code
+					 , conditiongroupspeo.numeric_value6
+					 , conditiongroupspeo.date_value6
+					 , conditiongroupspeo.alphanumeric_value6
+					 , conditiongroupspeo.determining_factor_code7
+					 , conditiongroupspeo.tax_parameter_code7
+					 , conditiongroupspeo.determining_factor_class7_code
+					 , conditiongroupspeo.operator7_code
+					 , conditiongroupspeo.numeric_value7
+					 , conditiongroupspeo.date_value7
+					 , conditiongroupspeo.alphanumeric_value7
+					 , conditiongroupspeo.determining_factor_code8
+					 , conditiongroupspeo.tax_parameter_code8
+					 , conditiongroupspeo.determining_factor_class8_code
+					 , conditiongroupspeo.operator8_code
+					 , conditiongroupspeo.numeric_value8
+					 , conditiongroupspeo.date_value8
+					 , conditiongroupspeo.alphanumeric_value8
+					 , conditiongroupspeo.determining_factor_code9
+					 , conditiongroupspeo.tax_parameter_code9
+					 , conditiongroupspeo.determining_factor_class9_code
+					 , conditiongroupspeo.operator9_code
+					 , conditiongroupspeo.numeric_value9
+					 , conditiongroupspeo.date_value9
+					 , conditiongroupspeo.alphanumeric_value9
+					 , conditiongroupspeo.determining_factor_code10
+					 , conditiongroupspeo.tax_parameter_code10
+					 , conditiongroupspeo.determining_factor_clas10_code
+					 , conditiongroupspeo.operator10_code
+					 , conditiongroupspeo.numeric_value10
+					 , conditiongroupspeo.date_value10
+					 , conditiongroupspeo.alphanumeric_value10
+					 , processresultdetail.alphanumeric_value
+				  from zx_condition_groups_b conditiongroupspeo
+					 , zx_process_results processresutspeo
+					 , zx_process_result_details processresultdetail
+				 where conditiongroupspeo.enabled_flag = 'Y'
+				   and processresutspeo.enabled_flag = 'Y'
+				   and conditiongroupspeo.condition_group_id = processresutspeo.condition_group_id
+				   and conditiongroupspeo.det_factor_templ_code in (select det_factor_templ_code
+																	  from zx_rules_b
+																	 where service_type_code = 'DET_TAX_BOX'
+																	   and enabled_flag = 'Y')
+				   and processresultdetail.result_id = processresutspeo.result_id) zx_condition
+	  order by zx_condition.priority
